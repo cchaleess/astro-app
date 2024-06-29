b804e65c1fe375dadbf75023ce50f4e14ff26cb0
Carlos <cmp@novicell.es>
2024-06-29 14:40:37 +0200

    Add prettier & detail movie & video trailer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'server', // SSR
+    // Integraciones y otras configuraciones
+    output: 'server', // Descomenta si necesitas SSR
 });
