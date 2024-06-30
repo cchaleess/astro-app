31981d0d385f0ddc802bb3dc87c1471538f299b4
Carlos <cmp@novicell.es>
2024-06-30 17:47:13 +0200

    netifly config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server' // SSR
   ,
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify()
 });
