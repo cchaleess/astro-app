0096abd911361e9b9c88a60d1ff6ab7b7f502edd
Carlos <cmp@novicell.es>
2024-06-30 15:59:37 +0200

    add Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 
 function Search() {
-  const [search, setSearch] = useState("");
+  const [value, setValue] = useState("");
+
+  const busqueda = (e) => {
+    setValue(e.target.value);
+  }
 
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button>Search</button>
-        <p>Search value: {search}</p>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <h1 className="text-center mb-4">Search movie: {value}</h1>
+          <form  action= {`/results/${value}`} method="POST" className="d-flex justify-content-center">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                value={value}
+                onChange={busqueda}
+              />
+              {/* <button className="btn btn-primary" type="submit">Search</button> */}
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
