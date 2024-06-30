bc619b943f5294839d86f711119c3b769dedd861
Carlos <cmp@novicell.es>
2024-06-30 17:39:35 +0200

    add files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server', // SSR
+  output: 'server' // SSR
+  ,
+  integrations: [tailwind()]
 });
